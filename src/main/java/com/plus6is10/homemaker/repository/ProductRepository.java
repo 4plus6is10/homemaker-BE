234191761dcf01578b90ea77c38a6c6f25827455
dongkyuu <gitdaram@gmail.com>
2022-07-12 09:23:56 +0900

    Mybatis 활용 완료 버전 업로드

---
+++ src/main/java/com/plus6is10/homemaker/repository/ProductRepository.java
@@ -0,0 +1,25 @@
+package com.plus6is10.homemaker.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.plus6is10.homemaker.model.dto.ProductDTO;
+
+@Mapper
+public interface ProductRepository {
+    
+    @Select("SELECT * FROM product")
+    List<ProductDTO> getAllProducts();
+
+    @Select("SELECT * FROM product WHERE name = #{name}")
+    ProductDTO getOneProduct(String name);
+    // RequestParam("name")
+
+    @Select("SELECT * FROM product WHERE category = #{category}")
+    List<ProductDTO> getProductsByCategory(String category);
+    
+}
