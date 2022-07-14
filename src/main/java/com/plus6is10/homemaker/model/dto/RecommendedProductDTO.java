c373b595d1ca7b1d0b9922ad8db2184cc8851957
dongkyuu <gitdaram@gmail.com>
2022-07-14 17:12:10 +0900

    2022-07-14 추천 제품 정보 추출 기능 추가 완료

---
+++ src/main/java/com/plus6is10/homemaker/model/dto/RecommendedProductDTO.java
@@ -0,0 +1,24 @@
+package com.plus6is10.homemaker.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RecommendedProductDTO {
+    
+    // private int seq;
+    private String searched_asin;
+    private String asin;
+    private String name;
+    private float price;
+    private String buylink;
+    private String imglink;
+    private String category;
+
+}
